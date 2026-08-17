a2f1e28931ea7daebf79b810cfeaeb64dc852d4b
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2026-08-18 01:03:21 +0530

    Keep existing Submit email template names

--- jobs/epic-cron/src/epic_cron/processors/submit/__init__.py
+++ jobs/epic-cron/src/epic_cron/processors/submit/__init__.py
@@ -7,11 +7,11 @@ from epic_cron.models.email_job import EmailJob
 
 
 MANAGEMENT_PLAN_SUBMISSION_CONFIRMATION_EMAIL_TEMPLATE = 'management_plan_submission_verification.html'
-MANAGEMENT_PLAN_UPDATE_REQUEST_CREATED_EMAIL_TEMPLATE = 'update_request_created.html'
+MANAGEMENT_PLAN_UPDATE_REQUEST_CREATED_EMAIL_TEMPLATE = 'management_plan_update_request_created.html'
 NEW_USER_INVITATION_EMAIL_TEMPLATE = 'new_user_invitation.html'
-MANAGEMENT_PLAN_SUBMISSION_NOTIFY_STAFF_EMAIL_TEMPLATE = 'submission_notify_staff.html'
+MANAGEMENT_PLAN_SUBMISSION_NOTIFY_STAFF_EMAIL_TEMPLATE = 'management_plan_submission_notify_staff.html'
 SUBMISSION_AWAITING_MANAGER_APPROVAL_EMAIL_TEMPLATE = 'submission_awaiting_manager_approval.html'
-MANAGEMENT_PLAN_RESUBMISSION_REQUEST_EMAIL_TEMPLATE = 'management_plan_resubmission_request.html'
+MANAGEMENT_PLAN_RESUBMISSION_REQUEST_EMAIL_TEMPLATE = 'resubmission_request.html'
 
 
 def process_submit_email(job: EmailJob) -> EmailDetails:
