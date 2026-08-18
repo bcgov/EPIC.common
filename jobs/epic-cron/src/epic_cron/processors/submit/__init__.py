f342252cc50691c8fc3df7214f9cd83a0ff75871
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2026-08-18 14:03:45 +0530

    Add withdrawn submission email template

--- jobs/epic-cron/src/epic_cron/processors/submit/__init__.py
+++ jobs/epic-cron/src/epic_cron/processors/submit/__init__.py
@@ -15,6 +15,7 @@ NEW_USER_INVITATION_COLLABORATOR_EMAIL_TEMPLATE = 'new_user_invitation_collabora
 MANAGEMENT_PLAN_SUBMISSION_NOTIFY_STAFF_EMAIL_TEMPLATE = 'management_plan_submission_notify_staff.html'
 SUBMISSION_AWAITING_MANAGER_APPROVAL_EMAIL_TEMPLATE = 'submission_awaiting_manager_approval.html'
 MANAGEMENT_PLAN_RESUBMISSION_REQUEST_EMAIL_TEMPLATE = 'resubmission_request.html'
+SUBMISSION_WITHDRAWN_CONFIRMATION_EMAIL_TEMPLATE = 'submission_withdrawn_confirmation.html'
 
 
 def process_submit_email(job: EmailJob) -> EmailDetails:
@@ -53,4 +54,5 @@ PROCESSORS: Dict[str, Callable[[EmailJob], EmailDetails]] = {
     MANAGEMENT_PLAN_SUBMISSION_NOTIFY_STAFF_EMAIL_TEMPLATE: process_submit_email,
     SUBMISSION_AWAITING_MANAGER_APPROVAL_EMAIL_TEMPLATE: process_submit_email,
     MANAGEMENT_PLAN_RESUBMISSION_REQUEST_EMAIL_TEMPLATE: process_submit_email,
+    SUBMISSION_WITHDRAWN_CONFIRMATION_EMAIL_TEMPLATE: process_submit_email,
 }
